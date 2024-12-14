37f333a7c7b00ab9f94d9e6fe9c94aeac0ab2650
test <test@DESKTOP-0H88GC0>
2024-12-14 22:24:49 +0300

    fix all KeyCell calculators

--- Tetris/TetrominoRotator.cs
+++ Tetris/TetrominoRotator.cs
@@ -14,7 +14,7 @@ namespace Tetris
             int matrixHeigth = item.Matrix.GetLength(0);
 
             int[,] tempMatrix = new int[matrixWidth, matrixHeigth];
-            int[] keyCell = item.calculateKeyCellPosition(item);
+            int[] keyCell = item.calculateKeyCellPosition(item, side);
 
             if (side)
             {
@@ -40,18 +40,91 @@ namespace Tetris
             return (tempMatrix, keyCell);
         }
 
-        public int[] DinamycKeyCell(Tetramino item)
+        /// <summary>
+        /// Calculate Key Cell for square form matrix
+        /// with fixed Centre (T and L shapes).
+        /// </summary>
+        /// <returns>New Key Cell Position</returns>
+        public int[] SquareKeyCell(Tetramino item, bool side)
         {
-            if (item.Matrix.GetLength(0) < item.Matrix.GetLength(1))
+            return GetSquareMatrixKeyCell(item, side);
+        }
+        private int[] GetSquareMatrixKeyCell(Tetramino item, bool side)
+        {
+            if (side) return GetClockWiseKeyCell(item);
+            else return GetCounterClockWiseKeyCell(item);
+        }
+        private int[] GetClockWiseKeyCell(Tetramino item)
+        {
+            //all possible key cells for square matrix
+            List<int[]> allKeyCells = GetAllKeyCells();
+            int itemKeyCellIndex = allKeyCells.FindIndex(cell => item.KeyCellPosition.SequenceEqual(cell));
+            bool isMatrixHorizontaly = item.Matrix.GetLength(0) < item.Matrix.GetLength(1);
+            bool isLastKeyCell = itemKeyCellIndex == allKeyCells.Count - 1;
+
+            if (isLastKeyCell)
             {
-                return new int[] { item.Matrix.GetLength(1) - (item.KeyCellPosition[1] + 1), item.KeyCellPosition[1] };
+                /* Key Cell {1, 1} is repeating two times.
+                 * Depending on orientation and turn side
+                 * we need to save it or switch to next 
+                 * key cell position.
+                 */
+                return isMatrixHorizontaly ?
+                    allKeyCells[itemKeyCellIndex - 1]
+                    :
+                    allKeyCells[itemKeyCellIndex];
             }
             else
             {
-                return new int[] { item.KeyCellPosition[0] - (item.Matrix.GetLength(0) - item.Matrix.GetLength(1)), item.KeyCellPosition[1] };
+                int nextIndex = (itemKeyCellIndex - 1) >= 0 ? (itemKeyCellIndex - 1) : allKeyCells.Count - 1;
+                return allKeyCells[nextIndex];
             }
         }
-        public int[] StaticKeyCell(Tetramino item)
+        private int[] GetCounterClockWiseKeyCell(Tetramino item)
+        {
+            //all possible key cells for square matrix
+            List<int[]> allKeyCells = GetAllKeyCells();
+            int itemKeyCellIndex = allKeyCells.FindIndex(cell => item.KeyCellPosition.SequenceEqual(cell));
+            bool isMatrixHorizontaly = item.Matrix.GetLength(0) < item.Matrix.GetLength(1);
+            bool isLastKeyCell = itemKeyCellIndex == allKeyCells.Count - 1;
+
+            if (isLastKeyCell)
+            {
+                /* Key Cell {1, 1} is repeating two times.
+                 * Depending on orientation and turn side
+                 * we need to save it or switch to next 
+                 * key cell position.
+                 */
+                return isMatrixHorizontaly ?
+                    allKeyCells[itemKeyCellIndex]
+                    :
+                    allKeyCells[0];
+            }
+            else
+            {
+                return allKeyCells[itemKeyCellIndex + 1];
+            }
+        }
+        private List<int[]> GetAllKeyCells() => new List<int[]>() {
+                new int[] { 0, 1 } ,
+                new int[] { 1, 0 } ,
+                new int[] { 1, 1 } //repeat two times
+            };
+
+        public int[] LineKeyCell(Tetramino item, bool side)
+        {
+            List<int[]> allKeyCells = new List<int[]>() { new int[] { 0, 1 }, new int[] { 2, 0 } };
+
+            return (item.KeyCellPosition.SequenceEqual(allKeyCells[0])) ? allKeyCells[1] : allKeyCells[0];
+        }
+
+        public int[] ZShapeKeyCell(Tetramino item, bool side)
+        {
+            List<int[]> allKeyCells = new List<int[]>() { new int[] { 0, 1 }, new int[] { 1, 1 } };
+
+            return (item.KeyCellPosition.SequenceEqual(allKeyCells[0])) ? allKeyCells[1] : allKeyCells[0];
+        }
+        public int[] StaticKeyCell(Tetramino item, bool side)
         {
             return item.KeyCellPosition;
         }
