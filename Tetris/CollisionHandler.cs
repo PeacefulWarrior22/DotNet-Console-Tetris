db955519ddbb8cb5daa986bd842b76631690608f
test <test@DESKTOP-0H88GC0>
2024-12-14 22:31:35 +0300

    fix IsInsideGameBoard function

--- Tetris/CollisionHandler.cs
+++ Tetris/CollisionHandler.cs
@@ -19,16 +19,25 @@ namespace Tetris
         }
         //public GameBoard GameBoard { get; }
 
-        public bool IsInsideGameBoard(int newTetraminoX)
+        public bool IsInsideGameBoard(int newTetraminoX, int newTetraminoY)
         {
             int tetraminoWidth = _tetramino.Matrix.GetLength(1);
             int tetraminoHeigth = _tetramino.Matrix.GetLength(0);
 
-            if (newTetraminoX >= 0 && newTetraminoX <= _gameBoard.GetWidth - tetraminoWidth)
+            int tetraminoMaxY = _gameBoard.GetHeigth - tetraminoHeigth;
+            int tetraminoMaxX = _gameBoard.GetWidth - tetraminoWidth;
+
+            bool isTetraminoVerticalyInside = newTetraminoY <= tetraminoMaxY;
+            bool isTetraminoHorizontlyInside = newTetraminoX >= 0 && newTetraminoX <= tetraminoMaxX;
+
+            if (isTetraminoHorizontlyInside && isTetraminoVerticalyInside)
             {
                 return true;
             }
-            else return false;
+            else
+            {
+                return false;
+            }
         }
         public CollisionHandler(Tetramino tetramino, GameBoard gameBoard)
         {
